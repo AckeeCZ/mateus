29bbbc3499d2c2f3838e8838160514fb2b50732a
Jiří Čermák <jiri.cermak@ackee.cz>
2019-04-25 23:27:24 +0200

    Add babel-plugin-transform-imports

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,22 @@
 module.exports = {
     presets: ['@babel/typescript', '@babel/env', '@babel/react'],
-    plugins: ['@babel/proposal-class-properties', '@babel/proposal-object-rest-spread'],
+    plugins: [
+        '@babel/proposal-class-properties',
+        '@babel/proposal-object-rest-spread',
+        [
+            'babel-plugin-transform-imports',
+            {
+                lodash: {
+                    // eslint-disable-next-line
+                    transform: 'lodash/${member}',
+                    preventFullImport: true,
+                },
+                antd: {
+                    transform: 'antd/lib/${member}',
+                    preventFullImport: true,
+                },
+            },
+        ],
+    ],
     ignore: ['**/__tests__/', 'src/**/*.d.ts'],
 };
